df4b719495312be6c4c9367fdd687d87deec4778
Eugeny Krimsky <uksan93@gmail.com>
2021-03-21 18:05:19 +0400

    made local storage saving

--- src/App.js
+++ src/App.js
@@ -6,73 +6,81 @@ import { Route } from 'react-router-dom'
 import Section from './components/sections/Section';
 
 const App = () => {
-  const [sections, setSection] = useState([
-      {
-          link: 'myquestions',
-          tittle: 'My Qestions',
-          messages: [
-            {
-              text: 'question 1'
-            },
-            {
-              text: 'question 2'
-            },
-          ],
-          newTextMessage: '',
-      },
-      {
-          link: 'messanger',
-          tittle: 'Messanger',
-          messages: [
-            {
-              text: 'message 1'
-            },
-            {
-              text: 'message 2'
-            },
-          ],
-          newTextMessage: ''
-      },
-      {
-          link: 'communityqa',
-          tittle: 'Community QA',
-          messages: [
-            {
-              text: 'qa 1'
-            },
-            {
-              text: 'qa 2'
-            },
-          ],
-          newTextMessage: ''
-      },
-      {
-          link: 'faq',
-          tittle: 'FAQ',
-          messages: [
-            {
-              text: 'answer 1'
-            },
-            {
-              text: 'answer 2'
-            },
-          ],
-          newTextMessage: ''
-      },
-  ])
+  const state = [
+    {
+        link: 'myquestions',
+        tittle: 'My Qestions',
+        messages: [
+          {
+            text: 'question 1'
+          },
+          {
+            text: 'question 2'
+          },
+        ],
+        newTextMessage: '',
+    },
+    {
+        link: 'messanger',
+        tittle: 'Messanger',
+        messages: [
+          {
+            text: 'message 1'
+          },
+          {
+            text: 'message 2'
+          },
+        ],
+        newTextMessage: ''
+    },
+    {
+        link: 'communityqa',
+        tittle: 'Community QA',
+        messages: [
+          {
+            text: 'qa 1'
+          },
+          {
+            text: 'qa 2'
+          },
+        ],
+        newTextMessage: ''
+    },
+    {
+        link: 'faq',
+        tittle: 'FAQ',
+        messages: [
+          {
+            text: 'answer 1'
+          },
+          {
+            text: 'answer 2'
+          },
+        ],
+        newTextMessage: ''
+  },
+  ]
 
-  window.sections = sections;
+  let storage = JSON.parse(localStorage.getItem('sections'));
+
+  let [sections, setSection] = useState(storage || state);
 
   function upgradeNewTextMessage(tittle, value) {
+    localStorage.clear();
+
     setSection(sections.map(section => {
       if (section.tittle === tittle) {
         section.newTextMessage = value;
       }
       return section
     }))
+
+    localStorage.setItem('sections', JSON.stringify(sections));
   }
 
   function addNewMessage(tittle) {
+    localStorage.clear();
+
     setSection(sections.map(section => {
       if (section.tittle === tittle) {
         section.messages.push({ text: section.newTextMessage });
@@ -80,6 +88,8 @@ const App = () => {
       }
       return section
     }))
+
+    localStorage.setItem('sections', JSON.stringify(sections));
   }
   
   const [isPressesBtn, setIsPresesBtn] = useState(false);
@@ -89,8 +99,11 @@ const App = () => {
   }
 
   function updateSections(newSection) {
+    localStorage.clear();
     setSection([...sections, newSection]);
     setIsPresesBtn(false);
+    
+    localStorage.setItem('sections', JSON.stringify(sections));
   }
 
   return (
@@ -112,7 +125,8 @@ const App = () => {
                 upgradeNewTextMessage={upgradeNewTextMessage}
                 addNewMessage={addNewMessage}
               />} 
-            />)}         
+            />
+          )}         
         </div>
     </BrowserRouter>
   );
