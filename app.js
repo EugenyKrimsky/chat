cb5af594d28e44420b163f8c7b6ebc6000c49061
eugenyk <uksan93@gmail.com>
2021-06-13 04:26:10 +0800

    connecting mongo

--- app.js
+++ app.js
@@ -9,12 +9,15 @@ const PORT = config.get('port') || 5000;
 async function start() {
     try {
         await mogoose.connect(config.get('mongoUri'), {
-
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
         });
+        app.listen(PORT, () => console.log(`start on port: ${PORT}`));
     } catch(e) {
         console.log('Server error', e.message);
         process.exit(1);
     }
 }
 
-app.listen(PORT, () => console.log(`start on port: ${PORT}`));
+start();
